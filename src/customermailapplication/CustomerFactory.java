0877d26c5b26b6301eea3a9fad2fc629b729a7ee
oreocream <ozonezanaruk@gmail.com>
2021-09-04 17:10:18 +0700

    62050194 Ass2 P.1

---
+++ src/customermailapplication/CustomerFactory.java
@@ -0,0 +1,17 @@
+package customermailapplication;
+
+public class CustomerFactory {
+    public static Customer CreateCustomer(String type){
+
+        if(type.equals("Regular")){
+            return new RegularCustomer();
+        }
+        else if(type.equals("Mountain")){
+            return new MountainCustomer();
+        }
+        else if(type.equals("Delinquent")){
+            return new DelinquentCustomer();
+        }
+            return null;
+    }
+}
--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -17,38 +17,29 @@ public class CustomerMailApplication {
     /**
      * @param args the command line arguments
      */
-    private Customer customer;
-    public void getCustomerTypeFromUser(String customerType) {
-        switch(customerType) {
-            case "Regular":
-                customer = new RegularCustomer();
-                break;
-            case "Mountain":
-                customer = new MountainCustomer();
-                break;
-            case "Delinquent":
-                customer = new DelinquentCustomer();
-                break;
-        }
-    }
+    private static Customer customer;
+
     public String generateMail() {
         return customer.createMail();
     }
+
     
     public static void main(String[] args) {
         CustomerMailApplication app = new CustomerMailApplication();
         Scanner inp = new Scanner(System.in);
         System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
         int type = inp.nextInt();
+        
+
         switch(type) {
             case 1:
-                app.getCustomerTypeFromUser("Regular");
+                customer = CustomerFactory.CreateCustomer("Regular");
                 break;
             case 2:
-                app.getCustomerTypeFromUser("Mountain");
+                customer = CustomerFactory.CreateCustomer("Mountain");
                 break;
             case 3:
-                app.getCustomerTypeFromUser("Delinquent");
+                customer = CustomerFactory.CreateCustomer("Delinquent");
                 break;
             
         }
